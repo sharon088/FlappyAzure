22794dbf5945e06d37abd510ed133c2cedd1ea5b
sharon088 <49710947+sharon088@users.noreply.github.com>
2025-02-10 19:25:28 +0200

    Update keyvault_values.sh

--- Terraform/terraform/keyvault/keyvault_values.sh
+++ Terraform/terraform/keyvault/keyvault_values.sh
@@ -5,6 +5,6 @@ VAULT_NAME=$1
 
 # Set secrets in the Key Vault dynamically using the passed name
 az keyvault secret set --vault-name "$VAULT_NAME" --name "AZURE-SQL-USERNAME" --value "adminuser"
-az keyvault secret set --vault-name "$VAULT_NAME" --name "AZURE-SQL-PASSWORD" --value "Hdo3Flappy@!"
+az keyvault secret set --vault-name "$VAULT_NAME" --name "AZURE-SQL-PASSWORD" --value "Enter your password here"
 az keyvault secret set --vault-name "$VAULT_NAME" --name "AZURE-SQL-SERVER" --value "azmysql"
 az keyvault secret set --vault-name "$VAULT_NAME" --name "AZURE-SQL-DATABASE" --value "azmydb"
